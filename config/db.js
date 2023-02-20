1d404a9ae7bd527d803fd34246a083010f87ff65
AthulBalaraman <athul2522001@gmail.com>
2023-02-20 18:40:47 +0530

    api created

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+
+const db = async()=>{
+  try {
+      const connection = await mongoose.connect('mongodb://0.0.0.0:27017/SOCIAL-MEDIA-APPLICATION', {
+          useNewUrlParser: true,
+          // useFindAndModify: false,
+          useUnifiedTopology: true
+      });
+      console.log(`MongoDb Connected Successfully : ${connection.connection.host} `);
+
+  } catch (error) {
+      console.log("==========>",error);
+  }
+}
+
+module.exports = db;
